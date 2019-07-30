4c047a9d14f1d918a2c8e4203c2f20358b4a25ea
Attila Egri-Nagy <attila@egri-nagy.hu>
2019-07-30 11:42:52 +0900

    just thinking about the board representation

--- src/lgo/board.clj
+++ src/lgo/board.clj
@@ -7,7 +7,15 @@
 ;; When connecting the newer chain is merged to the older one.
 
 
-(def symbols {:b \x :w \o nil \.})
+(def symbols {:b \X :w \O nil \bg
+              .})
 
-;; a chain, the stones order is not guaranteed since connecting could happen
+;; a chain is a hash-map , the stones order is not guaranteed since connecting could happen
+;; internally we use numbers for coordinates
 {:player :b, :stones [[3 4] [4 4]]}
+
+;;board position is a vector of chains + zobrist hash + whose move is that
+
+(declare put-stone ;; this puts a stone on a board position
+         legal-move? ;;decides whether the is legal or not on a given board position, ko?
+         )
