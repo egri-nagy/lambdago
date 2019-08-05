b3c6fa3502af36c145d0ca5ad76bcc2519051356
Attila Egri-Nagy <attila@egri-nagy.hu>
2019-08-05 11:51:21 +0900

    put-stone can append to chain now

--- src/lgo/board.clj
+++ src/lgo/board.clj
@@ -1,6 +1,7 @@
 (ns lgo.board
   "Functions for representing board state and its evolution."
-  (:require [lgo.util :refer [vec-rm]]))
+  (:require [lgo.util :refer [vec-rm]]
+            [kigen.position :refer [index]]))
 
 ;; The board position is stored as a vector of chains, in the order of creation.
 ;; A chain is represented by its oldest stone.
@@ -8,6 +9,8 @@
 
 
 (def symbols {:b \X :w \O nil \.})
+
+(def opposite {:b :w, :w :b})
 
 ;; a chain is a hash-map , the stones' order is not guaranteed since connecting could happen
 ;; internally we use numbers for coordinates
@@ -19,7 +22,7 @@
          legal-move? ;;decides whether the is legal or not on a given board position, ko?
          )
 ;;empty board
-{:width 2 :height 2 :chains []}
+
 
 (defn neighbours
   "Neighbours of a grid point considering the size of the board,
@@ -35,18 +38,48 @@
             points)))
 
 (defn containing-chain
-  [board [column row :as point]]
+  [{chains :chains} [column row :as point]]
   (filter
    (fn [chain] (let [stones (:stones chain)]
                  (not (empty? (filter (partial = point) stones)))))
-   board))
+   chains))
 
 (defn put-stone
-  "Places a single stone  on the board, updating the chain list."
-  [board [[column row] :as point] color]
+  "Places a single stone  on the board, updating the chain list.
+  For now this is used only for chain analysis, will not check legality of the
+  move fully."
+  [{width :width height :height chains :chains :as board}
+   [column row :as point]
+   color]
   (if (empty? (containing-chain board point))
+    ;;the stone is not on the board yet
+    (let [points (neighbours point width height)
+          connected-chains (mapcat (partial containing-chain board) points)
+          friendly-chains (filter (fn [chain] (= color (:player chain)))
+                                  connected-chains)
+          opponent-chains (filter (fn [chain] (= (opposite color) (:player chain)))
+                                  connected-chains)]
+      (cond
+        (empty? connected-chains)
+        (update board :chains
+                (fn [chains] (conj chains {:player color :stones [point]})))
 
+        (= 1 (count friendly-chains))
+        (update-in board
+                   [:chains (index chains (first friendly-chains)) :stones]
+                   (fn [v] (conj v point)))
+        ))
+    ;;illegal move, it's on the board already
     (do
       (println point " already on board")
       board))
   )
+
+(def empty-board {:width 3 :height 3 :chains []})
+(def signle-stone-to-append
+  (-> empty-board
+      (put-stone [1 2] :b)))
+(def two-stones-to-be-merged
+  (-> empty-board
+      (put-stone [1 2] :b)
+      (put-stone [3 2] :b)))
