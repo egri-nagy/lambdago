298a9cf6ffe72ec1ffed0bc9bc2803787552dd4d
Attila Egri-Nagy <attila@egri-nagy.hu>
2019-08-07 16:28:31 +0900

    rearranging, updating opponent chain added

--- src/lgo/board.clj
+++ src/lgo/board.clj
@@ -21,12 +21,13 @@
 
 ;;board position is a vector of chains + zobrist hash + whose move is that
 
-(declare put-stone ;; this puts a stone on a board position
+(declare empty-board ;; data structure for an empty board
+         put-stone ;; this puts a stone on a board position
          legal-move? ;;decides whether the is legal or not on a given board position, ko?
-         merge-chains
+         merge-chains ;;merging friendly chains
+         board-string ;; traditional ASCII rendering of the board
          )
 
-;;empty board
 (defn empty-board
   "Creates an empty board with the given dimensions."
   [width height]
@@ -34,6 +35,32 @@
    :height height
    :chains []
    :lookup {}}) ;; points to chains
+
+(defn add-chain
+  [board chain]
+  ;;adding it to the list of chains
+  (-> board
+      (update :chains (fn [chains] (conj chains chain)))
+      ;;updating the reverse lookup
+      (update :lookup (fn [m] (conj m [(first (:stones chain)) chain])))))
+
+(defn capture-chains
+  [board chains]
+  board)
+
+(defn update-chains
+  [{chains :chains :as board} ochains point]
+  (reduce (fn [brd chn]
+            (let [chn_index (index chains chn)
+                  brd2 (update-in brd
+                                  [:chains chn_index :liberties]
+                                  (partial remove #{point}))
+                  chn2 (nth (brd2 :chains) chn_index)]
+              (update brd2
+                      :lookup
+                      (fn [m] (into m (map (fn [pt] [pt chn2]) (:stones chn2)))))))
+          board
+          ochains))
 
 (defn put-stone
   "Places a single stone  on the board, updating the chain list.
@@ -64,33 +91,27 @@
           grouped_chains (group-by :color adj_chains)
           friendly_chains (grouped_chains color)
           opponent_chains (grouped_chains (opposite color))
-          to_be_captured (filter #(= 1 (count (:liberties %))) opponent_chains)]
-      (cond
-        ;; an individual stone, all neighbours are liberties
-        (= (count liberties) (count adjpts))
-        ;; we create new chain
-        (let [newchain {:color color
-                        :stones [point]
-                        :liberties liberties}]
-          ;;adding it to the list of chains
-          (-> board
-              (update :chains (fn [chains] (conj chains newchain)))
-              ;;updating the reverse lookup
-              (update :lookup (fn [m] (conj m [point newchain])))))
-
-        ;;when the neighbouring friendly chains share only the point as
-        ;;a single liberty, then the move is a self-capture
-        (= 1 (count (reduce union
-                            #{point}
-                            (map :liberties friendly_chains))))
-        (do
-          (println point "self-capture")
-          board)
-        :else
-        ;;trick is to add the connecting point as if it was a chain on its own
-        (merge-chains board (concat friendly_chains [{:color color
-                                                      :stones [point]
-                                                      :liberties liberties}]))))))
+          to_be_captured (filter #(= 1 (count (:liberties %))) opponent_chains)
+          to_be_updated (remove (set to_be_captured) opponent_chains)
+          updated_board (-> board
+                            (capture-chains to_be_captured)
+                            (update-chains to_be_updated point))]
+      (println updated_board)
+      ;; an individual stone, all neighbours are liberties
+      (if (empty? friendly_chains)
+        (if (= 1 (count (reduce union
+                                (union liberties #{point})
+                                (map :liberties friendly_chains))))
+          (do
+            (println friendly_chains point "self-capture")
+            board)
+          (add-chain updated_board {:color color
+                            :stones [point]
+                            :liberties liberties}))
+        (merge-chains updated_board
+                      (concat friendly_chains [{:color color
+                                                :stones [point]
+                                                :liberties liberties}]))))))
 
 (defn merge-chains
   "merging chains touching a point, heavy processing due to the
@@ -125,6 +146,12 @@
   (-> (empty-board 3 3)
       (put-stone [1 2] :b)
       (put-stone [3 2] :b)))
+
+(def atari
+  (-> (empty-board 2 2)
+      (put-stone [1 1] :w)
+      (put-stone [1 2] :b)))
+
 (def ponnuki
   (reduce (fn [board point]
             (put-stone board point :b))
