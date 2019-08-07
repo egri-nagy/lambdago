7c3925f5d6c976601fab25da9f678620a388f948
Attila Egri-Nagy <attila@egri-nagy.hu>
2019-08-07 12:21:56 +0900

    handling self-capture

--- src/lgo/board.clj
+++ src/lgo/board.clj
@@ -15,7 +15,7 @@
 
 ;; a chain is a hash-map , the stones' order is not guaranteed since connecting could happen
 ;; internally we use numbers for coordinates
-{:player :b, :stones [[3 4] [4 4]] :liberties [[][]]}
+{:color :b, :stones [[3 4] [4 4]] :liberties [[][]]}
 
 ;;board position is a vector of chains + zobrist hash + whose move is that
 
@@ -75,14 +75,18 @@
     ;;otherwise the stone is not on the board yet
     (let [opponent (opposite color)
           adjpts (neighbours point width height) ;;adjacent points, neighbours
-          chain_lookup (into {} (map lookup adjpts))
-          liberties (filter (complement chain_lookup) adjpts)
-          xyz (group-by chain_lookup adjpts)
-          friendly_pts (filter #(= color (:player (chain_lookup %))) adjpts)
-          enemy_pts (filter #(= opponent (:player (chain_lookup %))) adjpts)]
+          liberties (filter (complement lookup) adjpts)
+          ;; adjacent chains, no duplicates
+          adj_chains (filter identity (distinct (map lookup adjpts)))
+          grouped_chains (group-by :color adj_chains)
+          friendly_chains (grouped_chains color)]
+      (println liberties)
+      (println grouped_chains)
       (cond
-        (= (count liberties) (count adjpts)) ;; an individual stone, new chain
-        (let [newchain {:player color ;;creating this new chain
+        ;; an individual stone, all neighbours are liberties
+        (= (count liberties) (count adjpts))
+        ;; we create new chain
+        (let [newchain {:color color
                         :stones [point]
                         :liberties (set adjpts)}]
           ;;adding it to the list of chains
@@ -91,17 +95,31 @@
               ;;updating the reverse lookup
               (update :lookup (fn [m] (conj m [point newchain])))))
 
-        (= 1 (count friendly_pts)) ;; a single friendly chain
-        (let [chain_index (index chains (chain_lookup (first friendly_pts)))]
-            (-> board
-                (update-in [:chains  chain_index :stones]
-                           (fn [v] (conj v point)))
-                (update-in [:chains chain_index :liberties]
-                           (fn [s] (union )))))))))
+        ;;when the neighbouring friendly chains share only the point as
+        ;;a single liberty, then the move is a self-capture
+        (= 1 (count (reduce union
+                            #{point}
+                            (map :liberties friendly_chains))))
+        (do
+          (println point "self-capture")
+          board)
+        :else
+        (let [chain_index (index chains (first friendly_chains))]
+          (-> board
+              (update-in [:chains  chain_index :stones]
+                         (fn [v] (conj v point)))
+              (update-in [:chains chain_index :liberties]
+                         (fn [s] (union )))))))))
+
+(defn merge-chains
+  "merging chains touching a point "
+     [{chains :chains lookup :lookup :as board}
+      point]
+     )
 
 (def single-stone-to-append
-  (-> (empty-board 3 3)
-      (put-stone [1 2] :b)))
+  (-> (empty-board 1 2)
+      (put-stone [1 1] :b)))
 (def two-stones-to-be-merged
   (-> (empty-board 3 3)
       (put-stone [1 2] :b)
@@ -118,5 +136,5 @@
                    (for [c (range 1 (inc width))]
                      (concat
                       (for [r (range 1 (inc height))]
-                        (symbols (:player (lookup [c r]))))
+                        (symbols (:color (lookup [c r]))))
                       '(\newline))))))
