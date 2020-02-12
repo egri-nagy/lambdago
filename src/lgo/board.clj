de0ca2a8f8773023ff76d3f4b71909f78fea37c8
Attila Egri-Nagy <attila@egri-nagy.hu>
2020-02-12 11:43:24 +0900

    liberties are now separated into a different hash

--- src/lgo/board.clj
+++ src/lgo/board.clj
@@ -32,16 +32,16 @@
   {:width width
    :height height
    :chains [] ;; ordered by the age of the chains
+   :liberties {} ;; chains to liberties, since the liberties are more volatile
    :lookup {}}) ;; points to chains
 
 (defn single-stone-chain
   "Creates a single stone chain.
   A chain is a hash-map , the stones' order is not guaranteed since merging
   will happen."
-  [color point liberties]
+  [color point]
   {:color color
-   :stones [point]
-   :liberties liberties})
+   :stones [point]})
 
 (defn register-chain
   "updating the lookup table of the board by registering a given chain"
@@ -56,15 +56,24 @@
   [board n]
   (register-chain board ((:chains board) n)))
 
+(defn compute-liberties
+  "Computes liberties of a chain. Removing occupied points from its envelope."
+  [{width :width height :height lookup :lookup :as board} chain]
+  (let [e (envelope (:stones chain) width height)]
+    (set (remove lookup e))))
+
+
 (defn recompute-liberties
   "Recomputes liberties of a chain."
-  [{width :width height :height chains :chains lookup :lookup :as board} chain]
+  [{width :width height :height lookup :lookup :as board} chain]
   (let [e (envelope (:stones chain) width height)
-        chain_index (position (partial chain_eq_by_stones chain) chains)]
+                                        ;chain_index (position (partial chain_eq_by_stones chain) chains)
+        ]
     (-> board
-     (update-in [:chains  chain_index :liberties]
+     (update-in [:liberties  chain]
                 (fn [l] (union l (set (remove lookup e)))))
-     (register-chain-by-index chain_index)))) ;; why do we have to do this?
+     ;(register-chain-by-index chain_index)
+     ))) ;; why do we have to do this?
 
 (defn recompute-liberties-by-point
   "Recomputes liberties of a chain specified by one of its points."
@@ -78,12 +87,25 @@
 
 (defn add-chain
   "Adding a new chain to a board."
-  [board chain]
+  [board chain liberties]
   (-> board
       ;;adding it to the list of chains
-      (update :chains (fn [chains] (conj chains chain)))
+      (update :chains #(conj % chain))
+      ;;registering the liberties separately
+      (update :liberties #(conj % [chain liberties]))
       ;;updating the reverse lookup
       (register-chain chain)))
+
+(defn remove-chain
+  [board chain]
+  (-> board
+      (update :chains
+              (fn [chains] (vec-rm  chains
+                                    (index chains chain))))
+      (update :liberties #(dissoc % chain))
+      (update :lookup
+              (fn [m]
+                (apply dissoc m (:stones chain))))))
 
 (defn capture-chain
   [{lookup :lookup width :width height :height :as board}
@@ -94,12 +116,7 @@
                                  (map lookup
                                       (envelope stones width height))))]
     (-> board
-        (update :chains
-                (fn [chains] (vec-rm  chains
-                                      (index chains chain))))
-        (update :lookup
-                (fn [m]
-                  (apply dissoc m (:stones chain))))
+        (remove-chain chain)
         (update-liberties affected_chains))))
 
 (defn capture-chains
@@ -111,7 +128,7 @@
   (reduce (fn [brd chn]
             (let [chn_index (index chains chn)]
               (-> brd
-                  (update-in [:chains chn_index :liberties]
+                  (update-in [:liberties chn]
                              #(difference % #{point}))
                   (register-chain-by-index  chn_index) ;; we need to register since liberties change
                   )))
@@ -130,7 +147,7 @@
   3. when occupied by a friendly stone, chains
     a. get merged
     b. possibly captured."
-  [{width :width height :height chains :chains lookup :lookup :as board}
+  [{width :width height :height chains :chains lookup :lookup liberties :liberties :as board}
    [column row :as point]
    color]
   (if (lookup point)
@@ -145,20 +162,20 @@
           grouped_chains (group-by :color adj_chains)
           friendly_chains (grouped_chains color)
           opponent_chains (grouped_chains (opposite color))
-          to_be_captured (set (filter #(= 1 (count (:liberties %)))
+          to_be_captured (set (filter #(= 1 (count (liberties %)))
                                       opponent_chains))
           to_be_deced (remove (set to_be_captured) opponent_chains)
           liberties (set (filter #(or (nil? (lookup %))
                                       (to_be_captured (lookup %)))
                                  adjpts))
-          nchain (single-stone-chain  color point liberties)
+          nchain (single-stone-chain  color point)
           updated_board (-> board
                             (capture-chains to_be_captured)
                             (dec-liberties to_be_deced point)
-                            (add-chain nchain)
-                            (merge-chains friendly_chains nchain)
+                            (add-chain nchain liberties)
+                            (merge-chains (concat friendly_chains [nchain]))
                             (recompute-liberties-by-point point))]
-      (if (empty? (:liberties ((:lookup updated_board) point)))
+      (if (empty? ((:liberties updated_board) ((:lookup updated_board) point)))
         (do
           (println "self-capture")
           board)
@@ -170,39 +187,32 @@
   at this point we assume it is not a self-capture
   merging to the first"
   [{chains :chains lookup :lookup :as board}
-   to_be_merged
-   connector]
-  (if (empty? to_be_merged) ;;nothing to merge
+   cs]
+  (if (= 1 (count cs)) ;;nothing to merge
     board
-    (let [cs (concat to_be_merged [connector])
-          chain_indices (map (fn [c] (position (partial chain_eq_by_stones c) chains)) cs)
+    (let [chain_indices (map (fn [c] (position (partial chain_eq_by_stones c) chains)) cs)
           chain_index (first chain_indices)
           the_chain (first cs)
           upd_chain (reduce
                      (fn [ch1 ch2]
                        {:color (:color ch1)
-                        :stones (into (:stones ch1) (:stones ch2))
-                        :liberties (union (:liberties ch1) (:liberties ch2))})
-
+                        :stones (into (:stones ch1) (:stones ch2))})
                      the_chain
-                     (rest cs))
-          upd2 (update upd_chain :liberties
-                       (fn [l] (difference l (set (:stones connector)))))]
-      (when (nil? chain_index)
-        (println "chain index for merging" chain_index "\n chains in board" chains "\n chains we got fopr merging" cs))
+                     (rest cs))]
+
       (-> board
           (update-in [:chains  chain_index]
-                     (constantly upd2))
+                     (constantly upd_chain))
           (update :chains
-                  (fn [chains] (vec-rm-all chains (rest chain_indices))))
-          (recompute-liberties upd2)
-          (register-chain upd2)))))
+                  (fn [chains] (vec-rm-all chains chain_indices)))
+          (add-chain upd_chain (compute-liberties board upd_chain))
+          (register-chain upd_chain)))))
 
-;; (def ponnuki
-;;   (reduce (fn [board point]
-;;             (put-stone board point :b))
-;;           (empty-board 19 19)
-;;           [[1 2] [2 1] [3 2] [2 3]]))
+(def ponnuki
+  (reduce (fn [board point]
+            (put-stone board point :b))
+          (empty-board 19 19)
+          [[1 2] [2 1] [3 2] [2 3]]))
 
 ;;for the ASCII rendering of a board
 (def symbols {:b \X :w \O nil \.})
