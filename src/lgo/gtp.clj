ab9b2621336648ae117941890f8fcf393e70248e
Egri-Nagy Attila <attila@egri-nagy.hu>
2020-04-01 16:32:06 +0900

    working on the gtp loop

--- src/lgo/gtp.clj
+++ src/lgo/gtp.clj
@@ -1,9 +1,25 @@
 (ns lgo.gtp
-  "The GTP engine of LambdaGo.")
+  "The GTP engine of LambdaGo."
+  (:require [clojure.string :refer [split]]
+            [lgo.board :refer [empty-board put-stone]]))
 
 (defn gtp-loop
   []
-  (loop [input (read-line)]
-    (when-not (= "x" input)
-      (println input)
-      (recur (read-line)))))
+  (loop [input (read-line)
+         board nil]
+    (let [pieces (split input #" ")
+          command (first pieces)]
+      (if (= "quit" command)
+        board
+        (let [nboard
+              (case command
+                "boardsize" (do
+                              (println "=")
+                              (let [n (read-string (second pieces))]
+                                (empty-board n n)))
+                "genmove" (do
+                            (println "=")
+                            (let [n (read-string (second pieces))]
+                              (empty-board n n)))
+                board)]
+          (recur (read-line) nboard))))))
