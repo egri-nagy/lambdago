e6bca17b8dafc2347308a9f866419a4d98367db7
Egri-Nagy Attila <attila@egri-nagy.hu>
2020-04-15 14:52:46 +0900

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdago "2020.04.13"
+(defproject lambdago "2020.04.15"
   :description "Software package for the Igo Math course https://egri-nagy.github.io/igomath/"
   :url "https://github.com/egri-nagy/lambdago"
   :license {:name "MIT License"
