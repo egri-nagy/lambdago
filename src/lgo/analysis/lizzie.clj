33b43d24ca54dd6da3428b10eea28bb5d9405cc5
Egri-Nagy Attila <attila@egri-nagy.hu>
2020-08-10 14:53:26 +0900

    separating analysis code from data extraction

--- src/lgo/analysis/lizzie.clj
+++ src/lgo/analysis/lizzie.clj
@@ -13,7 +13,8 @@
             [lgo.sgf :refer [flat-list-properties
                              extract-properties
                              extract-single-value]]
-            [lgo.stats :refer [median mean]]))
+            [lgo.stats :refer [median mean]]
+            [lgo.analysis.oz :refer [game-report]]))
 
 (def B<->W {"B" "W", "W" "B"})
 
@@ -49,117 +50,6 @@
               :means meanz}))
          y (iterate inc 1)))) ;counting the moves from 1
 
-(defn unroll-scoremeans
-  "All score means from raw data. This is just unrolling the means vector
-  into separate rows."
-  [dat]
-  (mapcat
-   (fn [d]
-     (for [m (:means d)]
-       {:color (:color d)
-        :move (:move d)
-        :mean (if (= "B" (:color d))
-                m
-                (- m))}))
-   dat))
-
-(defn effects
-  "The score mean differences caused by the moves."
-  [dat]
-  (map (fn [[{c1 :color m1 :mean}
-             {m2 :mean v2 :move}]]
-         (let [eff (if (= c1 "B") ; need to negate for White
-                     (- m2 m1)
-                     (- (- m2 m1)))]
-           {:color c1 :effect eff :move v2}))
-       (partition 2 1 dat)))
-
-(defn choices
-  [dat]
-  (let [ps (partition 2 1 dat)]
-    (map (fn [[{c1 :color  m1 :mean mm :meanmean md :medianmean v1 :move}
-               {c2 :color m2 :mean v2 :move}]]
-           (if (= "B" c1)
-             {:color c1 :choice m2 :move v1 :average mm :median md :AI m1}
-             {:color c1 :choice (- m2) :move v1 :average (- mm) :median (- md) :AI (- m1)}
-             ))
-         ps)))
-
-(defn deviations
-  [effs]
-  (let [avg (mean (map :effect effs))]
-    (map (fn [{e :effect :as d}]
-           (into d [[:deviation (- e avg)]]))
-         effs)))
-
-
-(defn data-transform
-  "Prepares database for plotting in the same diagram. Fixed keys are copied,
-  then the variable ones are added as 'name' and its value under kw."
-  [db fixedkeys varkeys kw]
-  (mapcat (fn [row]
-            (let [fixedvals (into {} (map (fn [k] [k (k row)]) fixedkeys))]
-              (map (fn [k] (into fixedvals
-                                 [[:name (name k)] [kw (k row)]]))
-                   varkeys)))
-          db))
-
-;; Oz visualization functions producing vega-lite specifications
-(defn oz-effects
-  [e-d w t]
-  {:data {:values e-d}
-   :layer[{:encoding {:x {:field "move" :type "ordinal"}
-                        :y {:field "effect" :type "quantitative"} :color {:field "color" :type "nominal"}}
-             :mark "bar" :width w :title t}
-          {:encoding {
-                        :y {:field "effect" :type "quantitative" :aggregate "mean"} :color {:field "color" :type "nominal"}}
-             :mark "rule"}
-            ]})
-
-(defn oz-deviations
-  [e-d w t]
-  {:data {:values e-d}
-   :vconcat[{:encoding {:x {:field "move" :type "ordinal"}
-                        :y {:field "deviation" :type "quantitative"}}
-             :mark "bar" :width w :title t}]})
-
-(defn oz-normalized-effects
-  [e-d w t]
-  (let [N (count e-d)
-        cmsm (reductions + (map :effect e-d))
-        normalized (map (fn [d v] (into d [[:cumsum (/ v (/ (:move d) 2))]]))
-                     e-d cmsm)]
-    {:data {:values normalized}
-     :encoding {:x {:field "move" :type "quantitative"}
-                :y {:field "cumsum" :type "quantitative"} :color {:field "color" :type "nominal"}}
-     :mark "bar" :width w :title t}))
-
-(defn oz-effects-summary
-  [e-d]
-  {:data {:values e-d}
-   :title "Summary of effects"
-   :encoding {:x {:field "color" :type "nominal"}
-              :y {:field "effect" :type "quantitative"}}
-   :mark {:type "boxplot" :extent "min-max"}})
-
-(defn oz-all-scoremeans
-  [d w t]
-  {:data {:values d}
-   :width w
-   :title t
-   :encoding {:x {:field "move" :type "ordinal"}
-              :y {:field "mean" :type "quantitative"}}
-   :mark {:type "boxplot" :extent "min-max" :size 5}})
-
-
-(defn oz-choices
-  [c w t]
-  {:data {:values c}
-   :encoding {:x {:field "move" :type "ordinal"}
-              :y {:field "scoreMean" :type "quantitative"}
-              :color {:field "name" :type "nominal"}}
-   :mark {:type "line" :size 1}  :width w :title t })
-
 (defn sgf-report
   [sgf]
   (let [flp (flat-list-properties sgf)
@@ -167,44 +57,5 @@
         white (extract-single-value flp "PW")
         result (extract-single-value flp "RE")
         raw (raw-data flp)
-        all-sm (unroll-scoremeans raw)
-        effs-dat (effects raw)
-        dev-dat (deviations effs-dat)
-        cs (choices raw)
-        tcs (data-transform cs [:color :move]
-                            [:choice :median :AI :average] :scoreMean)
-        N (count effs-dat)
-        w (int (* 5.4 N))]
-    [:div
-     [:h1 (str "B: " black " W: " white) " R: " result]
-     [:p "Move numbers for score means indicate how many moves made before."]
-     [:vega-lite (oz-choices
-                  (filter #(= "B" (:color %)) tcs)
-                  w
-                  "Black's scoremean values")]
-     [:vega-lite (oz-choices
-                  (filter #(= "W" (:color %)) tcs)
-                  w
-                  "White's scoremean values")]
-     [:vega-lite (oz-all-scoremeans
-                  (filter #(= "B" (:color %)) all-sm)
-                  w
-                  "Black's all scoreMeans for variations")]
-     [:vega-lite (oz-all-scoremeans
-                  (filter #(= "W" (:color %)) all-sm)
-                  w
-                  "White's all scoreMeans for variations")]
-     [:vega-lite (oz-effects effs-dat w "Effects of moves")]
-     [:vega-lite (oz-effects (filter #(= "W" (:color %)) effs-dat) w "Effects of White's moves")]
-     [:vega-lite (oz-effects (filter #(= "B" (:color %)) effs-dat) w "Effects of Black's moves")]
-     [:vega-lite (oz-deviations (filter #(= "W" (:color %)) dev-dat) w "Deviations (distances from the mean) of White's moves")]
-     [:vega-lite (oz-deviations (filter #(= "B" (:color %)) dev-dat) w "Deviations of Black's moves")]
-     [:vega-lite
-      (oz-normalized-effects (filter #(= "W" (:color %)) effs-dat)  w
-                             "White's Cumulative sum of effects normalized by number of moves made")]
-     [:vega-lite
-      (oz-normalized-effects (filter #(= "B" (:color %)) effs-dat)  w
-                             "Black's Cumulative sum of effects normalized by number of moves made")]
-     [:div {:style {:display "flex" :flex-direction "row"}}
-      [:vega-lite (oz-effects-summary effs-dat)]]
-     [:p "Report generated by LambdaGo v" (System/getProperty "lambdago.version")]]))
+        title (str "B: " black " W: " white " R: " result)]
+    (game-report raw title)))
---
+++ src/lgo/analysis/oz.clj
@@ -0,0 +1,169 @@
+(ns lgo.analysis.oz
+  "Functions for working with the output of Lizzie after doing KataGo analysis.
+
+  Design decisions:
+  Internally all score values are from Black's perspective: positive means Black
+  win, negative means White win.
+  Move counter tells how many moves were made. Color tells whose turn is it.
+
+  The raw data is a hash-map with keys color, move, mean, meanmean, medianmean,
+  means.
+  "
+  (:require [clojure.string :as string]
+            [lgo.stats :refer [median mean]]))
+
+(defn unroll-scoremeans
+  "All score means from raw data. This is just unrolling the means vector
+  into separate rows."
+  [dat]
+  (mapcat
+   (fn [d]
+     (for [m (:means d)]
+       {:color (:color d)
+        :move (:move d)
+        :mean (if (= "B" (:color d))
+                m
+                (- m))}))
+   dat))
+
+(defn effects
+  "The score mean differences caused by the moves."
+  [dat]
+  (map (fn [[{c1 :color m1 :mean}
+             {m2 :mean v2 :move}]]
+         (let [eff (if (= c1 "B") ; need to negate for White
+                     (- m2 m1)
+                     (- (- m2 m1)))]
+           {:color c1 :effect eff :move v2}))
+       (partition 2 1 dat)))
+
+(defn choices
+  [dat]
+  (let [ps (partition 2 1 dat)]
+    (map (fn [[{c1 :color  m1 :mean mm :meanmean md :medianmean v1 :move}
+               {c2 :color m2 :mean v2 :move}]]
+           (if (= "B" c1)
+             {:color c1 :choice m2 :move v1 :average mm :median md :AI m1}
+             {:color c1 :choice (- m2) :move v1 :average (- mm) :median (- md) :AI (- m1)}
+             ))
+         ps)))
+
+(defn deviations
+  [effs]
+  (let [avg (mean (map :effect effs))]
+    (map (fn [{e :effect :as d}]
+           (into d [[:deviation (- e avg)]]))
+         effs)))
+
+
+(defn data-transform
+  "Prepares database for plotting in the same diagram. Fixed keys are copied,
+  then the variable ones are added as 'name' and its value under kw."
+  [db fixedkeys varkeys kw]
+  (mapcat (fn [row]
+            (let [fixedvals (into {} (map (fn [k] [k (k row)]) fixedkeys))]
+              (map (fn [k] (into fixedvals
+                                 [[:name (name k)] [kw (k row)]]))
+                   varkeys)))
+          db))
+
+;; Oz visualization functions producing vega-lite specifications
+(defn oz-effects
+  [e-d w t]
+  {:data {:values e-d}
+   :layer[{:encoding {:x {:field "move" :type "ordinal"}
+                        :y {:field "effect" :type "quantitative"} :color {:field "color" :type "nominal"}}
+             :mark "bar" :width w :title t}
+          {:encoding {
+                        :y {:field "effect" :type "quantitative" :aggregate "mean"} :color {:field "color" :type "nominal"}}
+             :mark "rule"}
+            ]})
+
+(defn oz-deviations
+  [e-d w t]
+  {:data {:values e-d}
+   :vconcat[{:encoding {:x {:field "move" :type "ordinal"}
+                        :y {:field "deviation" :type "quantitative"}}
+             :mark "bar" :width w :title t}]})
+
+(defn oz-normalized-effects
+  [e-d w t]
+  (let [N (count e-d)
+        cmsm (reductions + (map :effect e-d))
+        normalized (map (fn [d v] (into d [[:cumsum (/ v (/ (:move d) 2))]]))
+                     e-d cmsm)]
+    {:data {:values normalized}
+     :encoding {:x {:field "move" :type "quantitative"}
+                :y {:field "cumsum" :type "quantitative"} :color {:field "color" :type "nominal"}}
+     :mark "bar" :width w :title t}))
+
+(defn oz-effects-summary
+  [e-d]
+  {:data {:values e-d}
+   :title "Summary of effects"
+   :encoding {:x {:field "color" :type "nominal"}
+              :y {:field "effect" :type "quantitative"}}
+   :mark {:type "boxplot" :extent "min-max"}})
+
+(defn oz-all-scoremeans
+  [d w t]
+  {:data {:values d}
+   :width w
+   :title t
+   :encoding {:x {:field "move" :type "ordinal"}
+              :y {:field "mean" :type "quantitative"}}
+   :mark {:type "boxplot" :extent "min-max" :size 5}})
+
+
+(defn oz-choices
+  [c w t]
+  {:data {:values c}
+   :encoding {:x {:field "move" :type "ordinal"}
+              :y {:field "scoreMean" :type "quantitative"}
+              :color {:field "name" :type "nominal"}}
+   :mark {:type "line" :size 1}  :width w :title t })
+
+
+(defn game-report
+  [raw title]
+  (let [all-sm (unroll-scoremeans raw)
+        effs-dat (effects raw)
+        dev-dat (deviations effs-dat)
+        cs (choices raw)
+        tcs (data-transform cs [:color :move]
+                            [:choice :median :AI :average] :scoreMean)
+        N (count effs-dat)
+        w (int (* 5.4 N))]
+    [:div
+     [:h1 title]
+     [:p "Move numbers for score means indicate how many moves made before."]
+     [:vega-lite (oz-choices
+                  (filter #(= "B" (:color %)) tcs)
+                  w
+                  "Black's scoremean values")]
+     [:vega-lite (oz-choices
+                  (filter #(= "W" (:color %)) tcs)
+                  w
+                  "White's scoremean values")]
+     [:vega-lite (oz-all-scoremeans
+                  (filter #(= "B" (:color %)) all-sm)
+                  w
+                  "Black's all scoreMeans for variations")]
+     [:vega-lite (oz-all-scoremeans
+                  (filter #(= "W" (:color %)) all-sm)
+                  w
+                  "White's all scoreMeans for variations")]
+     [:vega-lite (oz-effects effs-dat w "Effects of moves")]
+     [:vega-lite (oz-effects (filter #(= "W" (:color %)) effs-dat) w "Effects of White's moves")]
+     [:vega-lite (oz-effects (filter #(= "B" (:color %)) effs-dat) w "Effects of Black's moves")]
+     [:vega-lite (oz-deviations (filter #(= "W" (:color %)) dev-dat) w "Deviations (distances from the mean) of White's moves")]
+     [:vega-lite (oz-deviations (filter #(= "B" (:color %)) dev-dat) w "Deviations of Black's moves")]
+     [:vega-lite
+      (oz-normalized-effects (filter #(= "W" (:color %)) effs-dat)  w
+                             "White's Cumulative sum of effects normalized by number of moves made")]
+     [:vega-lite
+      (oz-normalized-effects (filter #(= "B" (:color %)) effs-dat)  w
+                             "Black's Cumulative sum of effects normalized by number of moves made")]
+     [:div {:style {:display "flex" :flex-direction "row"}}
+      [:vega-lite (oz-effects-summary effs-dat)]]
+     [:p "Report generated by LambdaGo v" (System/getProperty "lambdago.version")]]))
