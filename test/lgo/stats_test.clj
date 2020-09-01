2a4b3cb0cf74e88e3c3e94b24dc3cd73b3b6d844
Attila Egri-Nagy <attila@egri-nagy.hu>
2020-09-01 11:47:20 +0900

    adding missing test for the stats functions

---
+++ test/lgo/stats_test.clj
@@ -0,0 +1,30 @@
+(ns lgo.stats-test
+  (:require [clojure.test :refer :all]
+            [lgo.stats :refer :all]))
+
+(deftest mean-test
+  (testing "Testing mean."
+    (is (= (mean [1 2 3]) 2))
+    (is (= (mean [1 -1]) 0))
+    (is (= (mean [2, 10, 21, 23, 23, 38, 38]) (/ 155 7)))))
+
+
+(deftest cmas-test
+  (testing "Testing cumulative moving averages."
+    (is (= (cmas [1 2 3]) [1 3/2 2]))
+    (is (= (cmas [1 -1]) [1 0]))
+    (is (= (cmas [2, 10, 21, 23, 23, 38, 38]) [2 6 11 14 79/5 39/2 155/7]))))
+
+(deftest median-test
+  (testing "Testing median."
+    (is (= (median [1 2 3]) 2))
+    (is (= (median [1 -1]) 0))
+    (is (= (median [38, 10, 21, 23, 23, 38, 2]) 23))
+    (is (== (median [38, 10, 21, 23, 22, 38, 2, 1]) 21.5))))
+
+(deftest KL-divergence-test
+  (testing "Testing Kullback-Leibler divergence."
+    (let [P [0.36 0.48 0.16] ; Wikipedia example
+          Q [1/3 1/3 1/3]]
+      (is (= (KL-divergence P Q) 0.0852996013183706))
+      (is (= (KL-divergence Q P) 0.09745500678538754)))))
