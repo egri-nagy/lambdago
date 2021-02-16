8417cc64a75d146014194cdfd3db22795ce8c0af
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-02-16 15:36:37 +0900

    indicating that it is just the game data, not including passed

--- src/lgo/analysis/processing.clj
+++ src/lgo/analysis/processing.clj
@@ -26,14 +26,14 @@
 
 (defn effects
   "The score mean differences caused by the moves."
-  [dat]
+  [gamedat]
   (map (fn [[{c1 :color m1 :mean}
              {m2 :mean v2 :move}]]
          (let [eff (if (= c1 "B") ; need to negate for White
                      (- m2 m1)
                      (- (- m2 m1)))]
            {:color c1 :effect eff :move v2}))
-       (partition 2 1 dat))) ; all pairs of the rows
+       (partition 2 1 gamedat))) ; all pairs of the rows
 
 (defn effects-with-cost-of-passing
   "The score mean differences caused by the moves."
