795162d5234ab7316fbe7a30469c8fb49ebfca7e
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-03-05 11:22:34 +0900

    better terminology, talk about stones in chains rather than points

--- src/lgo/chain.clj
+++ src/lgo/chain.clj
@@ -2,11 +2,11 @@
   "Functions for chains of stones."
   (:require [lgo.grid :refer [neighbours]]))
 
-(declare inside-points
-         boundary-points
+(declare inside-stones
+         boundary-stones
          envelope)
 
-(defn inside-points
+(defn inside-stones
   "Returns the stones that are 'inside' in the given group of stones.
   Inside stones are defined by the property of having all of its neighbours in
   the group.
@@ -18,20 +18,21 @@
               (every? S (neighbours point width height)))
             S)))
 
-(defn boundary-points
+(defn boundary-stones
   "Returns the stones that form the 'boundary' of the group.
-  Boundary is defined by the set minus the inside (see inside-points).
+  Boundary is defined by the set minus the inside (see inside-stones).
   This is meant for chains, however definition makes sense for general groups."
   [stones width height]
-  (remove (set (inside-points stones width height))
+  (remove (set (inside-stones stones width height))
           stones))
 
 (defn envelope
-  "Returns the points that are needed to surround the group of stones.
+  "Returns the points that need to be occupied by enemy stones to surround
+  the group of stones.
   Method: We compute all neighbours of the boundary, and remove the set
   of original stones from that."
   [stones width height]
-  (let [boundary (boundary-points stones width height)
+  (let [boundary (boundary-stones stones width height)
         fullneighbours (reduce (fn [r pt]
                                  (into r (neighbours pt width height )))
                                #{}
