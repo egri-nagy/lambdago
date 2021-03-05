0095462ad1d3b1a4de9246ffda5325ee0c823f57
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-03-05 11:41:41 +0900

    adding test for the envelope

--- test/lgo/chain_test.clj
+++ test/lgo/chain_test.clj
@@ -2,11 +2,20 @@
   (:require [clojure.test :refer :all]
             [lgo.chain :refer :all]))
 
+;; TODO: the test compare squences instead of sets, it works for now
+
 (deftest inside-vs-boundary-test
-  (testing "Testing the neighbours function for edges and corners."
+  (testing "Testing the inside and boundary stones."
     (is (= (inside-stones  [[1 2] [3 2] [2 1] [2 3] [2 2]] 3 3)
            '([2 2])))
     (is (= (inside-stones  [[1 2] [3 2] [2 1] [2 3] ] 3 3)
            '()))
     (is (= (boundary-stones  [[1 2] [3 2] [2 1] [2 3] [2 2]] 3 3)
            '([1 2] [3 2] [2 1] [2 3])))))
+
+(deftest envelope-test
+  (testing "Testing the envelope."
+    (is (= (envelope  [[1 2] [3 2] [2 1] [2 3] [2 2]] 3 3)
+           '( [3 3] [1 1] [1 3] [3 1] )))
+    (is (= (envelope  [[1 2] [3 2] [2 1] [2 3] ] 3 3)
+           '([2 2]  [3 3] [1 1] [1 3] [3 1])))))
