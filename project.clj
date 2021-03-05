9b6adde10cc6a640e1e95963912f725b3ad028a8
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-03-05 09:43:42 +0900

    updating deps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "none"
             :year 2019
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.bhauman/rebel-readline "0.1.4"]
                  [instaparse "1.4.10"]
                  [org.clojure/math.numeric-tower "0.0.4"]
@@ -17,7 +17,7 @@
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "0.3.13"]]
+            [jonase/eastwood "0.3.14"]]
   :main lgo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
