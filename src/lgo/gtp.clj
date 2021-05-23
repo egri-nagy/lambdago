3e75695c39c9812c380f722b596dbc5b06636f4b
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-05-23 20:39:42 +0900

    comments and separating conversion map

--- src/lgo/gtp.clj
+++ src/lgo/gtp.clj
@@ -1,7 +1,7 @@
 (ns lgo.gtp
   "The GTP engine of LambdaGo.
   The game is represented as a hash-map with keys :board, :moves, :history."
-  (:require [clojure.string :refer [split trim]]
+  (:require [clojure.string :refer [split trim join]]
             [lgo.board :refer [empty-board put-stone]]
             [trptcolin.versioneer.core :as version]
             [lgo.bot.random :refer [genmove]]))
@@ -18,28 +18,30 @@
 
 (def m {"B" :b "W" :w})
 
+(def numcoord (zipmap "ABCDEFGHJKLMNOPQRSTUVWXYZ" (range 1 25)))
+(def GTPcoord (zipmap (range 1 25) "ABCDEFGHJKLMNOPQRSTUVWXYZ"))
+
 (defn gtp-loop
+  "The main loop for listening to and executing GTP commands."
   []
-  (loop [input (read-line)
-         game {:moves [] :history #{}}]
-    (let [pieces (split input #" ")
-          command (first pieces)]
+  (loop [input (read-line) ;when starting, just read the first command and
+         game {:moves [] :history #{}}] ;create an empty game
+    (let [pieces (split input #" ") ;space is a separator for GTP commands
+          command (first pieces)] ;the first one is the command
       (if (= "quit" command)
-        game
-        (let [ngame
+        game ;when done, we return the game object
+        (let [ngame ;we create the next state, it might be the same
               (case command
                 "boardsize" (do ; creating a new board with the given size
                               (println "= \n")
-                              (let [n (read-string (second pieces))]
+                              (let [n (read-string (second pieces))] ;getting the size
                                 (conj game [:board (empty-board n n)])))
                 "clear_board" (do
                                 (println "= \n")
                                 (let [n (:width (:board game))] ;TODO what if the board is not initialized yet
                                   {:moves [] :history #{} :board (empty-board n n)}))
                 "version" (do
-                            (println "= "
-                                     (version/get-version "lambdago"
-                                                          "lambdago") "\n")
+                            (println "= " (version/get-version "lambdago" "lambdago") "\n")
                             game)
                 "komi" (do ;komi is ignored for the moment
                          (println "= \n")
@@ -49,9 +51,8 @@
                          game)
                 "play" (let [col  (m (trim (str (read-string (second pieces)))))
                              gtpmove (nth pieces 2)
-                             mm  (zipmap "ABCDEFGHJKLMNOPQRST" (range 1 25))
-                             x (mm (first gtpmove))
-                             y (read-string (apply str (rest gtpmove)))
+                             x (numcoord (first gtpmove))
+                             y (read-string (join (rest gtpmove)))
                              nboard (if (nil? x)
                                       (:board game)
                                       (put-stone (:board game) col [x,y]))]
@@ -69,10 +70,9 @@
                "genmove" (let [col  (m (trim (str (read-string (second pieces)))))
                                ng  (genmove game col)
                                m (last (:moves ng))
-                               mm  (zipmap (range 1 25) "ABCDEFGHJKLMNOPQRST")
                                out (if (= :pass m)
                                      "PASS"
-                                     (str (mm (first m)) (last m)))]
+                                     (str (GTPcoord (first m)) (last m)))]
                            (println "= " out "\n")
                            ng)
                 game)] ;default
