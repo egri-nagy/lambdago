5ae4eab3d28bcad361a581b9fe1bc65bb29a04b0
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-10-08 13:57:53 +0900

    documentation revision for random bot

--- src/lgo/bot/random.clj
+++ src/lgo/bot/random.clj
@@ -1,28 +1,29 @@
 (ns lgo.bot.random
-  "A simple bot playing random moves including eye-fills."
+  "A simple bot playing random moves including eye-fills.
+  It only passes when there are no legal moves left."
   (:require
    [lgo.board :refer [self-capture? put-stone opposite
                       board-string empty-points]]))
 
 (defn genmove
-  "Given a board position, the color of the player to make a move, and
-  a history of previous board positions, this returns a random legal move,
-  and the updated board position and the updated history.
-  Assumption is that we do it right, so there will be no need for rollback."
+  "Given a board position and the color of the player to make a move,
+  this makes a random legal move, and returns an updated board,
+  including the move sequence and history."
   [{board :board history :history :as game}
    color]
   ;; Initially the candidates are all the empty points.
+  ;; shuffle is used to create a random sequence
   (loop [cands (shuffle (empty-points board))]
     (if (empty? cands)
       (update game :moves #(conj % :pass)) ;passing if there are no options left
       (let [move (first cands)] ; just pick the first and try it
-        ;; first some checking for illegal/bad moves
+        ;; first checking legality
         (if (self-capture? board color move)
           (recur (rest cands)) ;if the move is not good, just try the rest
           (let [nboard (put-stone board color move)
                 current [(opposite color) (board-string nboard)]]
             (if (contains? history current)
-              (recur (rest cands)) ;situational superko
+              (recur (rest cands)) ;situational superko, another exit point
               (-> game
                   (update :board (constantly nboard))
                   (update :moves #(conj % move))
