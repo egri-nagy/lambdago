0028a49932c0fafeed7dad04bf66398103dfe659
Attila Egri-Nagy <attila@egri-nagy.hu>
2021-10-13 20:54:34 +0900

    review and comments

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdago "2021.08.24"
+(defproject lambdago "2021.10.13"
   :description "Software package for the Igo Math course https://egri-nagy.github.io/igomath/"
   :url "https://github.com/egri-nagy/lambdago"
   :license {:name "MIT License"
--- src/lgo/chain.clj
+++ src/lgo/chain.clj
@@ -1,5 +1,5 @@
 (ns lgo.chain
-  "Functions for chains of stones."
+  "General functions for chains of stones."
   (:require [lgo.grid :refer [neighbours]]))
 
 (declare inside-stones
@@ -14,13 +14,12 @@
   Color is not considered here."
   [stones width height]
   (let [S (set stones)]
-    (filter (fn [point]
-              (every? S (neighbours point width height)))
+    (filter (fn [point] (every? S (neighbours point width height)))
             S)))
 
 (defn boundary-stones
   "Returns the stones that form the 'boundary' of the group.
-  Boundary is defined by the set minus the inside (see inside-stones).
+  Boundary is defined by the set of stones minus its inside (see inside-stones).
   This is meant for chains, however definition makes sense for general groups."
   [stones width height]
   (remove (set (inside-stones stones width height))
@@ -28,14 +27,14 @@
 
 (defn envelope
   "Returns the points that need to be occupied by enemy stones to surround
-  the group of stones.
+  the group of stones. Note: this is not about a given board postion, just
+  a general computation - enemy stones are not checked.
   Method: We compute all neighbours of the boundary, and remove the set
   of original stones from that."
   [stones width height]
   (let [boundary (boundary-stones stones width height)
         fullneighbours (reduce (fn [r pt]
-                                 (into r (neighbours pt width height )))
+                                 (into r (neighbours pt width height)))
                                #{}
                                boundary)]
     (remove (set stones) fullneighbours)))
-
