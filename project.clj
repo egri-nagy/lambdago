153e137d919276aa07fb7df6388fbab3fd2cad9d
Attila Egri-Nagy <attila@egri-nagy.hu>
2022-01-19 08:55:09 +0900

    updating dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.bhauman/rebel-readline "0.1.4"]
                  [instaparse "1.4.10"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.numeric-tower "0.0.5"]
                  [kigen "19.08.05"]
                  [metasoarous/oz "1.6.0-alpha34"]
                  [org.clojure/data.json "2.4.0"]
@@ -17,7 +17,7 @@
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "0.9.9"]]
+            [jonase/eastwood "1.1.1"]]
   :main lgo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
