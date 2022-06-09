597f4322e7e8143d3c6460c5f77eb53daaeab2c1
Attila Egri-Nagy <attila@egri-nagy.hu>
2022-06-09 19:48:51 +0900

    new version to signpost the changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdago "2022.05.07"
+(defproject lambdago "2022.06.09"
   :description "Software package for the Igo Math course https://egri-nagy.github.io/igomath/"
   :url "https://github.com/egri-nagy/lambdago"
   :license {:name "MIT License"
