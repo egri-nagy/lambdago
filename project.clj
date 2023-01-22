77d020d4829cb7a324460039fd5fb0f596cd49d9
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-01-22 14:08:57 +0900

    just updating a plugin

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "1.2.4"]
+            [jonase/eastwood "1.3.0"]
             [fourtytoo/record-deps "0.3.3-SNAPSHOT"]]
   :main lgo.core
   :target-path "target/%s"
