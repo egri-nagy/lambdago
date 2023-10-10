e8a285dab3f89ac88d287c2284ce5910646382ae
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-10-10 10:42:22 +0900

    reviewing dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,6 @@
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.reader "1.3.6"] ;why? who needs this?
-                 [com.bhauman/rebel-readline "0.1.4"]
                  [instaparse "1.4.12"]
                  [org.clojure/math.numeric-tower "0.0.5"]
                  [org.clojure/data.json "2.4.0"]
@@ -17,11 +16,9 @@
   :plugins [[lein-cloverage "1.2.4"]
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
-            [lein-bikeshed "0.5.2"]
             [jonase/eastwood "1.4.0"]
             [fourtytoo/record-deps "0.3.3-SNAPSHOT"]
-            [cider/cider-nrepl "0.38.1"]]
+            [cider/cider-nrepl "0.39.0"]]
   :main lgo.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot [lgo.core]}}
-  :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]})
+  :profiles {:uberjar {:aot [lgo.core]}})
