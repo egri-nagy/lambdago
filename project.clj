2b2329fe0d24eb44be98937606432191c3a63e4e
Attila Egri-Nagy <attila@egri-nagy.hu>
2023-11-24 12:00:57 +0900

    added dependency + updates

--- project.clj
+++ project.clj
@@ -7,18 +7,19 @@
             :year 2022
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.reader "1.3.6"] ;why? who needs this?
+                 [org.clojure/tools.reader "1.3.7"] ;why? who needs this?
+                 [io.github.clojure/tools.build "0.9.6"]
                  [instaparse "1.4.12"]
                  [org.clojure/math.numeric-tower "0.0.5"]
                  [org.clojure/data.json "2.4.0"]
                  [meander/epsilon "0.0.650"]
-                 [org.scicloj/clay "2-alpha38"]]
+                 [org.scicloj/clay "2-alpha39"]]
   :plugins [[lein-cloverage "1.2.4"]
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [jonase/eastwood "1.4.2"]
             [fourtytoo/record-deps "0.3.3-SNAPSHOT"]
-            [cider/cider-nrepl "0.43.0"]]
+            [cider/cider-nrepl "0.43.3"]]
   :main lgo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot [lgo.core]}})
