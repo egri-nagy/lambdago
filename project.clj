4e3915fbdeb06babbdbaea19275ef62fa5c8d0b3
egri-nagy <attila@egri-nagy.hu>
2023-11-30 15:13:08 +0900

    updating deps - breaking changesin clay

--- project.clj
+++ project.clj
@@ -13,13 +13,13 @@
                  [org.clojure/math.numeric-tower "0.0.5"]
                  [org.clojure/data.json "2.4.0"]
                  [meander/epsilon "0.0.650"]
-                 [org.scicloj/clay "2-alpha39"]]
+                 [org.scicloj/clay "2-alpha41"]]
   :plugins [[lein-cloverage "1.2.4"]
             [lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [jonase/eastwood "1.4.2"]
             [fourtytoo/record-deps "0.3.3-SNAPSHOT"]
-            [cider/cider-nrepl "0.43.3"]]
+            [cider/cider-nrepl "0.44.0"]]
   :main lgo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot [lgo.core]}})
