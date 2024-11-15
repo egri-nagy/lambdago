a9fe95387ed26f929571c6fdd4ba6306b8188ad0
egri-nagy <attila@egri-nagy.hu>
2024-11-15 10:28:19 +0900

    updating project.clj

--- project.clj
+++ project.clj
@@ -6,20 +6,20 @@
             :url "none"
             :year 2022
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.reader "1.3.7"] ;why? who needs this?
-                 [io.github.clojure/tools.build "0.9.6"]
-                 [instaparse "1.4.12"]
-                 [org.clojure/math.numeric-tower "0.0.5"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/tools.reader "1.5.0"] ;why? who needs this?
+                 [io.github.clojure/tools.build "0.10.5"]
+                 [instaparse "1.5.0"]
+                 [org.clojure/math.numeric-tower "0.1.0"]
                  [org.clojure/data.json "2.5.0"]
                  [meander/epsilon "0.0.650"]
-                 [org.scicloj/clay "2-alpha49"]]
+                 [org.scicloj/clay "2-beta23"]]
   :plugins [[lein-cloverage "1.2.4"]
-            [lein-kibit "0.1.8"]
+            [lein-kibit "0.1.11"]
             [lein-ancient "0.7.0"]
-            [jonase/eastwood "1.4.2"]
+            [jonase/eastwood "1.4.3"]
             [fourtytoo/record-deps "0.3.3-SNAPSHOT"]
-            [cider/cider-nrepl "0.45.0"]]
+            [cider/cider-nrepl "0.50.2"]]
   :main lgo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot [lgo.core]}})
